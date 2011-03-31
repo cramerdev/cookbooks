49c68af9b7f33965891b30b08f562524470d4937
Nathan L Smith <nlloyds@gmail.com>
2011-03-31 13:26:59 -0500

    more options on wp app

--- application/recipes/wordpress.rb
+++ application/recipes/wordpress.rb
@@ -21,29 +21,35 @@ app = node.run_state[:current_app]
 include_recipe "apache2"
 
 directory app[:deploy_to] do
-  owner app[:owner]
-  group app[:group]
+  owner app[:owner] || node[:apache][:user]
+  group app[:group] || node[:apache][:user]
   mode "0755"
   action :create
 end
 
-template "wp-config.php" do
-  path "#{app[:deploy_to]}/shared/config/wp-config.php"
-  variables(
-    :db => app[:databases][node[:app_environment]].merge(:host => "localhost"),
-    :keys => app[:wordpress][:keys],
-    :url => "http://" + app[:domain_name][node[:app_environment]]
-  )
-  owner app[:owner]
-  group app[:group]
-  mode "0644"
+unless app[:skip_config]
+  template "wp-config.php" do
+    path "#{app[:deploy_to]}/shared/config/wp-config.php"
+    variables(
+      :db => app[:databases][node[:app_environment]].merge(:host => "localhost"),
+      :keys => app[:wordpress][:keys],
+      :url => "http://" + app[:domain_name][node[:app_environment]]
+    )
+    owner app[:owner]
+    group app[:group]
+    mode "0644"
+  end
 end
 
-web_app app[:id] do
-  docroot "#{app[:deploy_to]}/current"
+# Allow the app to be the default site
+app_conf = app[:default_site] ? "0000-#{app[:id]}" : app[:id]
+
+web_app app_conf do
+  docroot app[:docroot] || "#{app[:deploy_to]}/current"
   template "web_app.conf.erb"
   cookbook app[:id]
-  server_name app[:domain_name][node[:app_environment]]
+  ssl app[:ssl] || {}
+  server_name app[:domain_name][(node[:app_environment] || 'production')]
   log_dir node[:apache][:log_dir]
 end
 
