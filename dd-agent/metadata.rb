d47ad73a35a745f896b48882bf0a60681978c389
alq <alq666@gmail.com>
2011-04-05 15:11:40 -0400

    Recipe to deploy our agent via chef

---
+++ dd-agent/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Datadog, Inc."
+maintainer_email "package@datadoghq.com"
+license          "Apache"
+description      "Installs/Configures dd-agent"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "1.0.0"
+
---
+++ dd-agent/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: dd-agent
+# Recipe:: default
+#
+# Copyright 2011, Datadog, Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Skip to "HERE HERE HERE" below for user configurable data
+
+# Get the key
+execute "sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C7A7DA52" do
+  not_if "apt-key list | grep C7A7DA52"
+end
+
+# Add the repo
+cookbook_file "/etc/apt/sources.list.d/datadog.list" do
+    owner "root"
+    group "root"
+    mode 0755
+end
+
+# Update the repo
+execute "apt-get update"
+
+package "datadog-agent"
+
+service "datadog-agent" do
+    action :nothing
+    supports :restart => true
+end
+
+directory "/etc/dd-agent" do
+    owner "root"
+    group "root"
+    mode 0755
+end
+
+template "/etc/dd-agent/datadog.conf" do
+    owner "root"
+    group "root"
+    mode 0644
+    #
+    # HERE HERE HERE Replace the placeholder below with your api key
+    variables(:api_key => "REPLACE_ME_WITH_YOUR_API_KEY", :dd_url => "https://app.datadoghq.com")
+    # You are done! No need to touch the rest of the file
+    #
+    notifies :restart, "service[datadog-agent]", :immediately
+end
