fe6d9f962ad95c2d12c9e08827511e5ef118c93e
Nathan L Smith <nlloyds@gmail.com>
2011-04-13 14:57:05 -0500

    nginx/app fixes

--- application/recipes/rails_nginx_rvm_passenger.rb
+++ application/recipes/rails_nginx_rvm_passenger.rb
@@ -125,7 +125,7 @@ deploy_revision app[:id] do
   restart_command do
     execute 'restart passenger' do
       user app[:owner]
-      code "touch #{app[:deploy_to]}/current/tmp/restart.txt"
+      command "touch #{app[:deploy_to]}/current/tmp/restart.txt"
     end
   end
 
