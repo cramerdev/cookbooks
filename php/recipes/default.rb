b6f1bfcad12a849dc5ad5e42f0927c74e6646b91
Nathan L Smith <nlloyds@gmail.com>
2011-09-09 11:15:38 -0500

    Import php version 1.0.0

--- php/recipes/default.rb
+++ php/recipes/default.rb
@@ -1,9 +1,10 @@
 #
 # Author::  Joshua Timberman (<joshua@opscode.com>)
+# Author::  Seth Chisamore (<schisamo@opscode.com>)
 # Cookbook Name:: php
 # Recipe:: default
 #
-# Copyright 2009, Opscode, Inc.
+# Copyright 2009-2011, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,5 +17,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-include_recipe "php::php5"
+include_recipe "php::#{node['php']['install_method']}"
+
+# update the main channels
+php_pear_channel 'pear.php.net' do
+  action :update
+end
+
+php_pear_channel 'pecl.php.net' do
+  action :update
+end
