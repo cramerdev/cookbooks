448e0124d1e6819aed41c5c85bf43e617316a6d6
Nathan L Smith <nlloyds@gmail.com>
2012-05-24 11:55:00 -0500

    bump nginx passenger version

--- nginx/attributes/default.rb
+++ nginx/attributes/default.rb
@@ -34,4 +34,4 @@ default[:nginx][:worker_processes]   = cpu[:total]
 default[:nginx][:worker_connections] = 2048
 default[:nginx][:server_names_hash_bucket_size] = 64
 
-default[:nginx][:passenger][:version] = '3.0.11'
+default[:nginx][:passenger][:version] = '3.0.12'
