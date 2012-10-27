640cb0d261323464702f5a9274c038808274a920
Nathan L Smith <nlloyds@gmail.com>
2012-10-26 22:51:50 -0500

    rm backup cookbook

--- backup/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer       "Cramer Development, Inc."
-maintainer_email "sysadmin@cramerdev.com"
-license          "Apache 2.0"
-description      "Installs/Configures backup"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
-depends          "xml"
