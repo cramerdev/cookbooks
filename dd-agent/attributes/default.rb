ad08abd75db2d79f49bb6eac8fea42ea609a4a5e
Nathan L Smith <nlloyds@gmail.com>
2013-01-23 21:54:39 -0600

    remove dd-agent

--- dd-agent/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-# Uncomment this line and set the right value
-default[:datadog][:api_key] = "SET UP YOUR KEY HERE"
-default[:datadog][:url] = "https://app.datadoghq.com"
-default[:datadog][:repo] = "http://apt.datadoghq.com"
