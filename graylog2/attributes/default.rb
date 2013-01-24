d7cde288eef5ef6826d94b6f342287b792dd6ebf
Nathan L Smith <nlloyds@gmail.com>
2013-01-23 21:54:07 -0600

    remove graylog2

--- graylog2/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default.graylog2[:basedir] = "/opt/graylog2"
-default.graylog2[:webuiversion] = "0.9.5p2"
-default.graylog2[:collectionsize] = "50000000"
-default.graylog2[:date_format] = "%d.%m.%Y - %H:%M:%S"
